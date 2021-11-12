51d867e3d450106685684553bcb5db96672d2d1d
Akhlak Uzzalal <akhlakuzzalal@gmail.com>
2021-11-12 16:22:09 +0600

    second

--- index.js
+++ index.js
@@ -21,11 +21,66 @@ async function run() {
       await client.connect();
       const database = client.db('MotorBikeData');
       const bikesCollection = database.collection('bikes');
+      const users = database.collection('users');
+      const reviews = database.collection('reviews')
 
       app.post('/bikes', async (req, res) => {
          const bike = req.body;
          const result = await bikesCollection.insertOne(bike);
-         console.log('hit The Post ')
+         res.json(result);
+      });
+      // Post a review
+      app.post('/reviews', async (req, res) => {
+         const review = req.body;
+         const result = await reviews.insertOne(review);
+         res.json(result);
+      })
+
+
+      app.put('/users/:email', async (req, res) => {
+         const email = req.params.email;
+         console.log('put hit', email)
+         const filter = { email: email };
+         const option = { upsert: true };
+         const updateDoc = {
+            $set: {
+               role: "admin"
+            },
+         };
+         const result = await users.updateOne(filter, updateDoc, option);
+         res.json(result);
+      });
+      app.put('/users', async (req, res) => {
+         const user = req.body;
+         const filter = { email: user.email };
+         const options = { upsert: true };
+         const updateUser = {
+            $set: {
+               name: user.name,
+               img: user.img,
+            },
+         };
+         const result = await users.updateOne(filter, updateUser, options)
+         res.json(result);
+      });
+
+      // Get Bikes
+      app.get('/bikes', async (req, res) => {
+         const cursor = bikesCollection.find({});
+         const allBikes = await cursor.toArray();
+         res.json(allBikes);
+      });
+      // Get Reviews
+      app.get('/reviews', async (req, res) => {
+         const cursor = reviews.find({})
+         const allReviews = await cursor.toArray();
+         res.json(allReviews);
+      })
+
+      app.get('/users/:email', async (req, res) => {
+         const email = req.params.email;
+         const query = { email: email }
+         const result = await users.findOne(query);
          res.json(result);
       })
    }
